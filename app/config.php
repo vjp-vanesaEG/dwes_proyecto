596cd8a44c0cccaab2c3742d27eb473c5613ca76
1DAW Vanesa Espada González <vespadag01@iesvjp.es>
2024-11-21 13:15:37 +0100

    c

--- app/config.php
+++ app/config.php
@@ -5,7 +5,7 @@ return ['database' =>
     'name' => 'proyecto',
     'username' => 'user',
     'password' => 'user',
-    'connection' => 'mysql:host=dwes.local',
+    'connection' => 'mysql:host=localhost',
     'options' => [
         PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
